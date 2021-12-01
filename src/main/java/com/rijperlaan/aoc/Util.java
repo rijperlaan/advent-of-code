67b0817b19a62c1c3357e2b84d45211fe438da26
rijperlaan <robinyperlaan@hotmail.com>
2021-12-01 17:23:34 +0100

    Add day 1

---
+++ src/main/java/com/rijperlaan/aoc/Util.java
@@ -0,0 +1,43 @@
+package com.rijperlaan.aoc;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Util {
+    public static List<Integer> readFileAsIntegers(String fileName) {
+        List<Integer> list = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line = br.readLine();
+
+            while (line != null) {
+                list.add(Integer.parseInt(line));
+                line = br.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+    public static List<String> readFileAsStrings(String fileName) {
+        List<String> list = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line = br.readLine();
+
+            while (line != null) {
+                list.add(line);
+                line = br.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+}
---
+++ src/main/java/com/rijperlaan/aoc/day1/Day1.java
@@ -0,0 +1,44 @@
+package com.rijperlaan.aoc.day1;
+
+import com.rijperlaan.aoc.Util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day1 {
+
+    public static void main(String[] args) {
+        part1();
+        part2();
+    }
+
+    public static void part1() {
+        List<Integer> input = Util.readFileAsIntegers("inputs/day1.txt");
+        int increases = 0;
+        for (int i = 0; i < input.size() - 1; i++) {
+            if (input.get(i) < input.get(i + 1)) {
+                increases++;
+            }
+        }
+
+        System.out.println("increases p1 = " + increases);
+    }
+
+    public static void part2() {
+        List<Integer> input = Util.readFileAsIntegers("inputs/day1-2.txt");
+        List<Integer> sums = new ArrayList<>();
+
+        for (int i = 0; i < input.size() - 2; i++) {
+            sums.add(input.get(i) + input.get(i + 1) + input.get(i + 2));
+        }
+
+        int increases = 0;
+        for (int i = 0; i < sums.size() - 1; i++) {
+            if (sums.get(i) < sums.get(i + 1)) {
+                increases++;
+            }
+        }
+
+        System.out.println("increases p2 = " + increases);
+    }
+}
